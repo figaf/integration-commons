e5b37e6b1fd3826bf22294c740a0a4cd01d5d839
Kostas T. Charalambous <kostascharalambous@outlook.com.gr>
2024-03-26 13:49:05 +0200

    IRT-4316 In BTP give information about missing Cloud Connectors if PI connection

--- src/main/java/com/figaf/integration/common/enums/TypeOfService.java
+++ src/main/java/com/figaf/integration/common/enums/TypeOfService.java
@@ -1,5 +1,17 @@
 package com.figaf.integration.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum TypeOfService {
-    CONNECTIVITY, DESTINATION
+
+    CONNECTIVITY("Connectivity Service"),
+
+    DESTINATION("Destination Service");
+
+    private final String title;
+
+    TypeOfService(String title) {
+        this.title = title;
+    }
 }
