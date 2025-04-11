b21268b008f3bba404b3312162fedd40a63d8b8d
Arsenii Istlentev <ais@figaf.com>
2025-04-11 14:20:03 +0300

    IRT-4954 Move parseDate method from cpi-api

--- src/main/java/com/figaf/integration/common/utils/Utils.java
+++ src/main/java/com/figaf/integration/common/utils/Utils.java
@@ -4,12 +4,50 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.json.JSONObject;
 
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+
 import static java.lang.String.format;
 
 /**
  * @author Arsenii Istlentev
  */
 public class Utils {
+
+    private static final DateTimeFormatter DATE_FORMATTER = new DateTimeFormatterBuilder()
+        .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+        .optionalStart()
+        .appendPattern(".SSS")
+        .optionalEnd()
+        .optionalStart()
+        .appendLiteral('Z')
+        .optionalEnd()
+        .toFormatter()
+        .withZone(ZoneId.of("GMT"));
+
+    public static Date parseDate(String date) {
+        try {
+            if (date == null) {
+                return null;
+            }
+            if (date.matches(".*Date\\(.*\\).*")) {
+                return new Timestamp(
+                    Long.parseLong(
+                        date.replaceAll("[^0-9]", "")
+                    )
+                );
+            } else {
+                ZonedDateTime zdt = ZonedDateTime.parse(date, DATE_FORMATTER);
+                return Date.from(zdt.toInstant());
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Can't parse date: ", ex);
+        }
+    }
 
     public static String optString(JSONObject json, String key) {
         if (json.isNull(key)) {
